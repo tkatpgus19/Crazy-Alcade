8d80c52a108696be17db1c03affba42741d2d301
limsubinn <mymyx02@naver.com>
2024-01-23 10:30:21 +0900

    Feat: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/eni/backend/CrazyAlcadeApplication.java
@@ -0,0 +1,13 @@
+package com.eni.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyAlcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyAlcadeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eni/backend/CrazyAlcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyAlcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
