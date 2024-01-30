94a92d9e46e017d975e15d32aab72bfef90b7052
박창준 <qmak01@naver.com>
2024-01-30 16:10:25 +0900

    Design : 게임 화면 디자인 다듬기

--- frontend/src/pages/Game/Footer.js
+++ frontend/src/pages/Game/Footer.js
@@ -8,9 +8,24 @@ import ActionButton from "./components/ActionButton";
 const Footer = ({ onSave, onRun, onSubmit }) => {
   return (
     <div className={styles.footer}>
-      <ActionButton color="#3498db" text="임시 저장" onClick={onSave} />
-      <ActionButton color="#27ae60" text="코드 실행" onClick={onRun} />
-      <ActionButton color="#e74c3c" text="코드 제출" onClick={onSubmit} />
+      <ActionButton
+        className={styles.button}
+        color="#3498db"
+        text="임시 저장"
+        onClick={onSave}
+      />
+      <ActionButton
+        className={styles.button}
+        color="#27ae60"
+        text="코드 실행"
+        onClick={onRun}
+      />
+      <ActionButton
+        className={styles.button}
+        color="#e74c3c"
+        text="코드 제출"
+        onClick={onSubmit}
+      />
     </div>
   );
 };
