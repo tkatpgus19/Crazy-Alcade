7188465c949cb94710f870e47735fcb7109e4ea2
sehyun <germs1020@gmail.com>
2024-01-31 15:37:42 +0900

    Comment: WebSockConfig 파일에 주석 추가

--- backend/src/main/java/com/eni/backend/common/config/WebSockConfig.java
+++ backend/src/main/java/com/eni/backend/common/config/WebSockConfig.java
@@ -11,12 +11,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+
+        // 메시지 구독 요청 url
         config.enableSimpleBroker("/sub");
+
+        // 메시지 발행 요청 url
         config.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        // stomp 접속 주소 url
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*")
                 .withSockJS();
     }
