08a520be75239cfa48b5881406d6fe06afb3450f
limsubinn <mymyx02@naver.com>
2024-01-31 14:29:03 +0900

    Feat: 자동 빌드 테스트 - 3

--- backend/src/main/java/com/eni/backend/TestController.java
+++ backend/src/main/java/com/eni/backend/TestController.java
@@ -1,15 +1,18 @@
 package com.eni.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("")
     public String test() {
+        log.info("돼라돼라돼라");
         return "ci-cd test";
     }
 
