5d9fc86aa42ccedb241f18d5dc499338592b758d
hyunjik <hjl0122@gmail.com>
2024-02-01 10:56:48 +0900

    Add: 아이템 상점 modal 생성 및 방 만들기 modal 수정

--- frontend/src/pages/Main/CreateRoomModal.js
+++ frontend/src/pages/Main/CreateRoomModal.js
@@ -13,21 +13,33 @@ const CreateRoomModal = ({ closeModal, createRoom }) => {
     timeLimit: "",
     languages: { java: false, python: false },
     codeReview: false,
+    codeReviewReject: false,
   });
 
   // 입력값이 변경될 때 호출되는 핸들러 함수를 정의합니다.
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    setRoomData({
-      ...roomData,
+    setRoomData((prevData) => ({
+      ...prevData,
       [name]: type === "checkbox" ? checked : value,
-    });
+    }));
   };
 
   // 폼을 제출할 때 호출되는 핸들러 함수를 정의합니다.
   const handleSubmit = () => {
     createRoom(roomData); // 방 만들기 함수 호출
     closeModal(); // 모달 닫기 함수 호출
+  };
+
+  // 풀이 언어 변경 핸들러 함수
+  const handleChangeLanguage = (e, language) => {
+    setRoomData((prevData) => ({
+      ...prevData,
+      languages: {
+        ...prevData.languages,
+        [language]: e.target.checked,
+      },
+    }));
   };
 
   // 렌더링 결과를 반환합니다.
@@ -72,14 +84,19 @@ const CreateRoomModal = ({ closeModal, createRoom }) => {
         onChange={handleChange}
       />
 
-      {/* 시간 제한 입력란 */}
+      {/* 시간 제한 드롭다운 */}
       <label>시간 제한:</label>
-      <input
-        type="text"
+      <select
         name="timeLimit"
         value={roomData.timeLimit}
         onChange={handleChange}
-      />
+      >
+        <option value="">선택하세요</option>
+        <option value="1h">1시간</option>
+        <option value="1h30m">1시간 30분</option>
+        <option value="2h">2시간</option>
+        <option value="2h30m">2시간 30분</option>
+      </select>
 
       {/* 풀이 언어 체크박스 */}
       <label>풀이 언어:</label>
@@ -91,7 +108,7 @@ const CreateRoomModal = ({ closeModal, createRoom }) => {
             name="languages"
             value="java"
             checked={roomData.languages.java}
-            onChange={handleChange}
+            onChange={(e) => handleChangeLanguage(e, "java")}
           />
         </label>
         <label>
@@ -101,34 +118,32 @@ const CreateRoomModal = ({ closeModal, createRoom }) => {
             name="languages"
             value="python"
             checked={roomData.languages.python}
-            onChange={handleChange}
+            onChange={(e) => handleChangeLanguage(e, "python")}
           />
         </label>
       </div>
 
       {/* 코드 리뷰 체크박스 */}
-      <div>
-        <label>코드 리뷰: </label>
-        <div>
-          <label>
-            o
-            <input
-              type="checkbox"
-              name="codeReviewApprove"
-              checked={roomData.codeReviewApprove}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            x
-            <input
-              type="checkbox"
-              name="codeReviewReject"
-              checked={roomData.codeReviewReject}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
+      <label>코드 리뷰:</label>
+      <div className={styles.codeReviewCheckbox}>
+        <label>
+          o
+          <input
+            type="checkbox"
+            name="codeReview"
+            checked={roomData.codeReviewApprove}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          x
+          <input
+            type="checkbox"
+            name="codeReviewReject"
+            checked={roomData.codeReviewReject}
+            onChange={handleChange}
+          />
+        </label>
       </div>
 
       {/* 추가적인 방 정보에 대한 유사한 입력 필드를 추가하세요 */}
