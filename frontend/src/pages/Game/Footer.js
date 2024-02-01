c5b00ac607cfd7e8fb8f7f396d85328d142c42fe
박창준 <qmak01@naver.com>
2024-02-01 18:08:32 +0900

    Design : 게임화면 - 하단 아이템 창 생성

--- frontend/src/pages/Game/Footer.js
+++ frontend/src/pages/Game/Footer.js
@@ -1,13 +1,28 @@
 // Footer.js
-
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Footer.module.css";
+import ItemButton from "./components/ItemButton"; // ItemButton 컴포넌트 임포트
 import ActionButton from "./components/ActionButton";
 
-const Footer = ({ onSave, onRun, onSubmit }) => {
+const Footer = ({ onSave, onRun, onSubmit, onUseItem }) => {
+  const handleItemUse = (item) => {
+    // 사용할 아이템에 따라 처리 로직 추가
+    onUseItem(item);
+  };
+
   return (
     <div className={styles.footer}>
+      {/* 내 아이템 영역 */}
+      <div className={styles.itemContainer}>
+        <div className={styles.itemHeader}>내 아이템</div>
+        {/* 각각의 아이템 버튼을 ItemButton 컴포넌트로 대체 */}
+        <ItemButton itemName="아이템1" onUseItem={onUseItem} />
+        <ItemButton itemName="아이템2" onUseItem={onUseItem} />
+        <ItemButton itemName="아이템3" onUseItem={onUseItem} />
+      </div>
+
+      {/* 기존 버튼들 유지 */}
       <ActionButton
         className={styles.button}
         color="#3498db"
@@ -34,6 +49,7 @@ Footer.propTypes = {
   onSave: PropTypes.func.isRequired,
   onRun: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  onUseItem: PropTypes.func.isRequired,
 };
 
 export default Footer;
