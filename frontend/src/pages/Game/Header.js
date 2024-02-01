7f06e29203173afe2e7baacf130284964fd06f8d
박창준 <qmak01@naver.com>
2024-02-01 14:22:03 +0900

    fix conflicts

--- frontend/src/pages/Game/Header.js
+++ frontend/src/pages/Game/Header.js
@@ -1,23 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styles from "./Header.module.css";
-
-
-import RoomTitle from "../../components/titles/RoomTitle.js";
+import "./Header.css"; // 필요에 따라 CSS 파일의 경로를 수정하세요.
+import ExitButton from "../../components/buttons/ExitButton.js"; // Import the ExitButton component
+import RoomTitle from "../../components/titles/RoomTitle.js"; // Import the ExitButton component
 import LanguageBox from "./components/LanguageBox.js";
 import Timer from "./components/Timer.js";
-import ExitButton from "../../components/buttons/ExitButton.js";
-
-const Header = ({ roomTitle, language, initialTime, onExitClick }) => {
+const Header = ({ roomTitle, language, remainingTime, onExitClick }) => {
   return (
-    <div className={styles.header}>
-      <div className={styles.roomInfo}>
-        <RoomTitle title={roomTitle} />
+    <div className="header">
+      <div className="room-info">
+        <RoomTitle title={roomTitle} /> {/* RoomTitle 컴포넌트 사용 */}{" "}
         <LanguageBox language={language} />
       </div>
-      <div className={styles.timerAndExit}>
-        <Timer initialTime={initialTime} />
-        <ExitButton onClick={onExitClick} />
+      <div className="timer-and-exit">
+        <Timer remainingTime={remainingTime} />
+        <ExitButton onClick={onExitClick} /> {/* Use ExitButton component */}
       </div>
     </div>
   );
@@ -27,7 +24,7 @@ const Header = ({ roomTitle, language, initialTime, onExitClick }) => {
 Header.propTypes = {
   roomTitle: PropTypes.string.isRequired,
   language: PropTypes.string.isRequired,
-  initialTime: PropTypes.number.isRequired,
+  remainingTime: PropTypes.number.isRequired,
   onExitClick: PropTypes.func.isRequired,
 };
 
