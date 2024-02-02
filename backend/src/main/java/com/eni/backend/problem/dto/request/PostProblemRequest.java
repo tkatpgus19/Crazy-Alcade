f1e87eb73bed4a037c4017f6772c4e944094f477
limsubinn <mymyx02@naver.com>
2024-02-02 18:56:03 +0900

    Style: 필요 없는 import문 제거

--- backend/src/main/java/com/eni/backend/problem/dto/request/PostProblemRequest.java
+++ backend/src/main/java/com/eni/backend/problem/dto/request/PostProblemRequest.java
@@ -1,8 +1,5 @@
 package com.eni.backend.problem.dto.request;
 
-import com.eni.backend.common.entity.ProblemPlatform;
-import com.eni.backend.problem.entity.Problem;
-import com.eni.backend.problem.entity.Tier;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
--- backend/src/main/java/com/eni/backend/problem/dto/request/PostTestcaseRequest.java
+++ backend/src/main/java/com/eni/backend/problem/dto/request/PostTestcaseRequest.java
@@ -1,6 +1,5 @@
 package com.eni.backend.problem.dto.request;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
