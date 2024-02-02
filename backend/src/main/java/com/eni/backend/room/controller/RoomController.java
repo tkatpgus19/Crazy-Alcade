f5047b51fc14f9355bbef65a89d14461e0e58728
sehyun <germs1020@gmail.com>
2024-02-02 16:31:22 +0900

    Feat: 노말전 방, 아이템 방 조회 api 추가

--- backend/src/main/java/com/eni/backend/room/controller/RoomController.java
+++ backend/src/main/java/com/eni/backend/room/controller/RoomController.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.*;
@@ -36,4 +39,34 @@ public class RoomController {
         return new ResponseEntity<>(roomDto.getRoomId(), HttpStatus.OK);
     }
 
+    // 노멀전 방 리스트 조회
+    @GetMapping("/normal")
+    public ResponseEntity<?> getNormalRoomList(){
+        log.warn("노말룸 정보: {}", roomService.getNormalRoomList());
+        return new ResponseEntity<>(roomService.getNormalRoomList(), HttpStatus.OK);
+    }
+
+    // 아이템전 방 리스트 조회
+    @GetMapping("/item")
+    public ResponseEntity<?> getItemRoomList(){
+        return new ResponseEntity<>(roomService.getItemRoomList(), HttpStatus.OK);
+    }
+
+    // 방 입장
+    @MessageMapping("/chat/enterUser")
+    public void enterUser(@Payload ChatDto chat, SimpMessageHeaderAccessor headerAccessor) {
+        // 채팅방에 유저 추가 및 UserUUID 반환
+        String userUUID = roomService.addUser(chat);
+
+        // 반환 결과를 socket session 에 userUUID 로 저장
+        headerAccessor.getSessionAttributes().put("userUUID", userUUID);
+        headerAccessor.getSessionAttributes().put("roomId", chat.getRoomId());
+        headerAccessor.getSessionAttributes().put("roomType", chat.getRoomType());
+
+        chat.setMessage(chat.getSender() + " 님 입장!!");
+        template.convertAndSend("/sub/chat/room/" + chat.getRoomId(), chat);
+
+        template.convertAndSend("/sub/normal/room-list", roomService.getNormalRoomList());
+        template.convertAndSend("/sub/item/room-list", roomService.getItemRoomList());
+    }
 }
