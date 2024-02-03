6dfb04aa48177fb174b8cc94558d482579384479
limsubinn <mymyx02@naver.com>
2024-02-03 17:35:26 +0900

    Fix: CodeStatus 변경

--- backend/src/main/java/com/eni/backend/problem/entity/CodeStatus.java
+++ backend/src/main/java/com/eni/backend/problem/entity/CodeStatus.java
@@ -1,5 +1,18 @@
 package com.eni.backend.problem.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum CodeStatus {
-    SUCCESS, FAIL
+    SUCCESS("맞았습니다."),
+    FAIL("틀렸습니다."),
+    COMPILE_ERROR("컴파일 에러"),
+    RUNTIME_ERROR("런타임 에러"),
+    TIME_OVER("시간 초과"),
+    MEMORY_OVER("메모리 초과"),
+    ;
+
+    private final String status;
 }
