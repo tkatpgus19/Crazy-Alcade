6a5de2666778fe6b8415504493b8b62101d930b2
limsubinn <mymyx02@naver.com>
2024-02-12 14:12:07 +0900

    Fix: 임시 cors 허용

--- backend/src/main/java/com/eni/backend/problem/entity/Problem.java
+++ backend/src/main/java/com/eni/backend/problem/entity/Problem.java
@@ -7,10 +7,12 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("*")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
