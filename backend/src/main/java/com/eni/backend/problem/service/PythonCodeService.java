4d9ed156b78d3232ea5d7a1e11187a3bd5bccb6a
limsubinn <mymyx02@naver.com>
2024-02-12 22:16:36 +0900

    Fix: 충돌 해결

--- backend/src/main/java/com/eni/backend/problem/service/PythonCodeService.java
+++ backend/src/main/java/com/eni/backend/problem/service/PythonCodeService.java
@@ -109,7 +109,7 @@ public class PythonCodeService {
             // 결과 반환
             return CodeSubmitResponse.of(codeStatus, results);
         }
-        
+
         // 실행
         testcases = testcaseRepository.findAllByProblemIdAndIsHidden(problem.getId(), isHidden);
         List<CodeExecuteDto> results = new ArrayList<>();
