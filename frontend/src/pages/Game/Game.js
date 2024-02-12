6a3914bf668535118c455954daada842dbb1fa81
박창준 <qmak01@naver.com>
2024-02-13 03:02:33 +0900

    Fix : 게임화면 - 타이머 요청 삭제

--- frontend/src/pages/Game/Game.js
+++ frontend/src/pages/Game/Game.js
@@ -132,11 +132,11 @@ function Game() {
     fetchUserInfo();
 
     // 타이머 온 -> 방장만 요청.
-    if (roomInfo.master === nickname) {
-      axios.get(
-        `${process.env.REACT_APP_BASE_URL}/rooms/set-timer?roomId=${roomId}`
-      );
-    }
+    // if (roomInfo.master === nickname) {
+    //   axios.get(
+    //     `${process.env.REACT_APP_BASE_URL}/rooms/set-timer?roomId=${roomId}`
+    //   );
+    // }
   }, []);
 
   // 게임 모드에 따른 배경 화면 설정
