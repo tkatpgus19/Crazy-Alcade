1c5e96dae0710eb401638346480e4fdc20a5ebf2
limsubinn <mymyx02@naver.com>
2024-02-12 15:53:22 +0900

    Fix: 자바 코드 실행 - 전체 결과 추가

--- backend/src/main/java/com/eni/backend/problem/dto/response/CodeExecuteResponse.java
+++ backend/src/main/java/com/eni/backend/problem/dto/response/CodeExecuteResponse.java
@@ -1,26 +1,29 @@
 package com.eni.backend.problem.dto.response;
 
+import com.eni.backend.problem.entity.CodeStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+
+import java.util.List;
 
 @ToString
 @Getter
 public class CodeExecuteResponse {
 
-    private Integer testcaseNo;
-    private String codeStatus;
+    private String allResult;
+    private List<CodeExecuteDto> tcResult;
 
     @Builder
-    private CodeExecuteResponse(Integer testcaseNo, String codeStatus) {
-        this.testcaseNo = testcaseNo;
-        this.codeStatus = codeStatus;
+    private CodeExecuteResponse(String allResult, List<CodeExecuteDto> tcResult) {
+        this.allResult = allResult;
+        this.tcResult = tcResult;
     }
 
-    public static CodeExecuteResponse of(Integer testcaseNo, String codeStatus) {
+    public static CodeExecuteResponse of(CodeStatus codeStatus, List<CodeExecuteDto> tcResult) {
         return builder()
-                .testcaseNo(testcaseNo)
-                .codeStatus(codeStatus)
+                .allResult(codeStatus.getStatus())
+                .tcResult(tcResult)
                 .build();
     }
 
