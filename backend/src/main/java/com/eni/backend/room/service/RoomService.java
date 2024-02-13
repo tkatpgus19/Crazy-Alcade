eadb3df379b813eafc53797614ef0e934c838792
sehyun <germs1020@gmail.com>
2024-02-13 20:36:45 +0900

    Fix: 방장 위임 기능 버그 수정

--- backend/src/main/java/com/eni/backend/room/service/RoomService.java
+++ backend/src/main/java/com/eni/backend/room/service/RoomService.java
@@ -147,10 +147,9 @@ public class RoomService {
         if(roomId != null) {
             RoomDto room = roomRepository.getRoomById(roomId);
             if(room != null) {
-                room.setUserCnt(room.getUserCnt() - 1);
                 String user = room.getUserList().get(userUUID);
 
-                if (room.getReadyList().get(user).equals("MASTER") && room.getUserCnt() != 0) {
+                if (room.getReadyList().get(user).equals("MASTER") && room.getUserCnt() != 1) {
                     room.getReadyList().remove(user);
                     Map.Entry<String, String> firstEntry = room.getReadyList().entrySet().iterator().next();
                     room.getReadyList().replace(firstEntry.getKey(), "MASTER");
@@ -161,6 +160,8 @@ public class RoomService {
                 room.getUserList().remove(userUUID);
                 log.info("User exit : " + user);
 
+                room.setUserCnt(room.getUserCnt() - 1);
+
                 // builder 어노테이션 활용
                 ChatDto chat = ChatDto.builder()
                         .type(ChatDto.MessageType.LEAVE)
@@ -169,11 +170,12 @@ public class RoomService {
                         .build();
                 template.convertAndSend("/sub/chat/room/" + roomId, chat);
 
-                if (getUserStatus(roomId) != null) {
-                    template.convertAndSend("/sub/room/" + roomId + "/status", getUserStatus(roomId));
-                }
                 if (room.getUserCnt() == 0) {
                     roomRepository.getRoomMap().remove(roomId);
+                }
+                clearRooms();
+                if (getUserStatus(roomId) != null) {
+                    template.convertAndSend("/sub/room/" + roomId + "/status", getUserStatus(roomId));
                 }
             }
             template.convertAndSend("/sub/normal/room-list", getSortedRoomList("normal", null, null, null, false, 1));
