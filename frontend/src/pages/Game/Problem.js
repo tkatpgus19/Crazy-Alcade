684cc47dcd69d2b55ae0a2d546a97b361e36d0b3
박창준 <qmak01@naver.com>
2024-02-14 09:12:37 +0900

    Fix : Game.js, Problem.js 날라간 데이터 확인

--- frontend/src/pages/Game/Problem.js
+++ frontend/src/pages/Game/Problem.js
@@ -10,26 +10,28 @@ const Problem = ({ problemId, problemTier }) => {
 
   useEffect(() => {
     const getData = async () => {
-      const apiUrl = `${process.env.REACT_APP_BASE_URL}/problems/${problemId}`;
+      if (!problemData) {
+        const apiUrl = `${process.env.REACT_APP_BASE_URL}/problems/${problemId}`;
 
-      try {
-        const response = await fetch(apiUrl);
+        try {
+          const response = await fetch(apiUrl);
 
-        if (!response || !response.ok) {
-          throw new Error("서버 응답이 올바르지 않습니다.");
+          if (!response || !response.ok) {
+            throw new Error("서버 응답이 올바르지 않습니다.");
+          }
+
+          const data = await response.json();
+          setProblemData(data.result);
+          setLoading(false);
+        } catch (error) {
+          console.error("데이터를 불러오는 중에 문제가 발생했습니다.", error);
+          setProblemData(null);
+          setLoading(false);
         }
-
-        const data = await response.json();
-        setProblemData(data.result);
-        setLoading(false);
-      } catch (error) {
-        console.error("데이터를 불러오는 중에 문제가 발생했습니다.", error);
-        setProblemData(null);
-        setLoading(false);
       }
-    };
 
-    getData();
+      getData();
+    };
   }, []);
 
   if (loading) {
@@ -45,59 +47,6 @@ const Problem = ({ problemId, problemTier }) => {
   }
 
   return renderProblem(problemData);
-};
-
-const renderProblem = (data) => {
-  console.log(data.description);
-
-  return (
-    <div className={styles.problemBox}>
-      <div className={styles.problem}>
-        <div className={styles.stickyHeader}>
-          {/* <h2>와 <h3> 태그에 custom 클래스 적용 */}
-          <h2 className={styles.customH2}>
-            {data.no}. {data.title}
-          </h2>
-          <div className={styles.limitsContainer}>
-            <div className={styles.limit}>
-              <FontAwesomeIcon icon={faClock} /> <span>{data.time}</span>
-            </div>
-            <div className={styles.limit}>
-              <FontAwesomeIcon icon={faMicrochip} /> <span>{data.memory}</span>
-            </div>
-          </div>
-        </div>
-        <hr className={styles.customHr} />
-        <h3>문제</h3>
-        <p className={styles.customP}>{data.description}</p>
-
-        <hr className={styles.customHr} />
-        <h3>입력</h3>
-        <p>{data.input}</p>
-        <hr />
-        <h3>출력</h3>
-        <p>{data.output}</p>
-
-        <hr />
-        <h3>예제</h3>
-        <div className={styles.examplesContainer}>
-          {data.examples &&
-            data.examples.map((example, index) => (
-              <div key={index} className={styles.exampleRow}>
-                <div className={styles.exampleBlock}>
-                  <h4>예제 입력 {index + 1}</h4>
-                  <pre className={styles.exampleContent}>{example.input}</pre>
-                </div>
-                <div className={styles.exampleBlock}>
-                  <h4>예제 출력 {index + 1}</h4>
-                  <pre className={styles.exampleContent}>{example.output}</pre>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
-    </div>
-  );
 };
 
 Problem.propTypes = {
