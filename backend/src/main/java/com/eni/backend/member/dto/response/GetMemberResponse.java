6eb01bd211118ca4800e0085ef0df1307708f81d
cyeon1118 <cyeon1118@naver.com>
2024-02-15 13:19:13 +0900

    Fix: exp 출력 기능 추가 및 레벨업 기능 수정

--- backend/src/main/java/com/eni/backend/member/dto/response/GetMemberResponse.java
+++ backend/src/main/java/com/eni/backend/member/dto/response/GetMemberResponse.java
@@ -13,26 +13,29 @@ public class GetMemberResponse {
     private String nickname;
     private String profile;
     private Integer levelId;
-    private Integer exp;
+    private Integer tempExp;
+    private Integer expLimit;
     private Integer coin;
     private List<GetMemberItemListResponse> memberItemList;
 
     @Builder
-    private GetMemberResponse(String nickname, String profile, Integer levelId, Integer exp, Integer coin, List<GetMemberItemListResponse> memberItemList) {
+    private GetMemberResponse(String nickname, String profile, Integer levelId, Integer tempExp, Integer expLimit, Integer coin, List<GetMemberItemListResponse> memberItemList) {
         this.nickname = nickname;
         this.profile = profile;
         this.levelId = levelId;
-        this.exp = exp;
+        this.tempExp = tempExp;
+        this.expLimit = expLimit;
         this.coin = coin;
         this.memberItemList = memberItemList;
     }
 
-    public static GetMemberResponse from(Member member, List<GetMemberItemListResponse> memberItemList) {
+    public static GetMemberResponse from(Member member, Integer tempExp, Integer expLimit, List<GetMemberItemListResponse> memberItemList) {
         return builder()
                 .nickname(member.getNickname())
                 .profile(member.getProfile())
                 .levelId(member.getLevel().getId())
-                .exp(member.getExp())
+                .tempExp(tempExp)
+                .expLimit(expLimit)
                 .coin(member.getCoin())
                 .memberItemList(memberItemList)
                 .build();
